131aae0c4f7aa35a7788cdb74b8cfb22f4785e0e
Sky <anvandp18@gmail.com>
2018-10-03 11:58:16 -0700

    we want to edit artist form

--- tunr_django/tunr/urls.py
+++ tunr_django/tunr/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	path('artists/<int:pk>', views.artist_detail, name="artist_detail"), #just like artists/:id in nodejs
 	path('songs/<int:pk>', views.song_detail, name="song_detail"),
 	path('artists/new', views.artist_create, name="artist_create"),
+	path('artist/<int:pk>/edit', views.artist_edit, name="artist_edit"),
 ]
