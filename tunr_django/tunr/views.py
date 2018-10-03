1a12751c3cecc1b01e43c8c3a565e3d94d25d1fa
Sky <anvandp18@gmail.com>
2018-10-03 09:56:10 -0700

    create artlist_list.html

--- tunr_django/tunr/views.py
+++ tunr_django/tunr/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Artist
 
 # Create your views here.
+def artist_list(request):
+	artists = Artist.objects.all()
+	return render(request, 'artist_list.html', {'artists': artists})
